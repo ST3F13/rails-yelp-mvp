e24cde9e87f85f9013d9e7c8520de51c4f28333d
stephane godet <st3f13@orange.fr>
2021-05-26 17:01:54 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+mcdonalds = { name: "McDonald's", address: "Marseille", phone_number: 0400000000, category: "chinese" }
+kfc = { name: "KFC", address: "La Seyne-sur-Mer", phone_number: 0400000000, category: "italian" }
+burgerking = { name: "Burger King", address: "Aubagne", phone_number: 0400000000, category: "japanese" }
+pizzahut = { name: "Pizza Hut", address: "Toulon", phone_number: 0400000000, category: "french" }
+otacos = { name: "O'Tacos", address: "Les Pennes-Mirabeau", phone_number: 0400000000, category: "belgian" }
+
+[mcdonalds, kfc, burgerking, pizzahut, otacos].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
